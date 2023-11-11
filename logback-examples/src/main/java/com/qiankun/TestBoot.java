87c444e8f5aa2e93bd613314925baf0c8e846544
tiankun <847415859@qq.com>
2023-11-11 10:28:54 +0800

    init

---
+++ logback-examples/src/main/java/com/qiankun/TestBoot.java
@@ -0,0 +1,16 @@
+package com.qiankun;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @Description:
+ * @Date : 2023/11/11 10:18
+ * @Auther : tiankun
+ */
+public class TestBoot {
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(TestBoot.class);
+        logger.info("aaaa");
+    }
+}
